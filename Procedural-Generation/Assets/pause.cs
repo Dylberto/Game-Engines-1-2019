e8aaf06100a548c32557f675217524e2870d20b6
Dylberto <dylang565@gmail.com>
2019-12-18 07:22:05 +0000

    Game

---
+++ Procedural-Generation/Assets/pause.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class pause : MonoBehaviour
+{
+    public bool paused = false;
+    public static bool canPause = true;
+    public GameObject pause_menu_ui;
+    
+
+
+  
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && canPause)
+        {
+            if (!paused)
+            {
+                pause_game();
+            }
+        }
+
+        if(SceneManager.GetActiveScene().buildIndex == 0)
+        {
+            Time.timeScale = 1f;
+            paused = false;
+            resume();
+        }
+    }
+
+    void pause_game()
+    {
+        Cursor.visible = true;
+        pause_menu_ui.SetActive(true);
+        Time.timeScale = 0f;
+        paused = true;
+        Cursor.lockState = CursorLockMode.None;
+        AudioListener.pause = true;
+    }
+
+    public void resume()
+    {
+        pause_menu_ui.SetActive(false);
+        Time.timeScale = 1f;
+        paused = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        AudioListener.pause = false;
+
+    }
+
+
+    public void quit()
+    {
+        Application.Quit();
+    }
+}
